e5760b9f4fe6888b0785a997b855db3455b62ed0
AfraiCat <111137425+AfraiCat@users.noreply.github.com>
2024-01-25 21:49:33 +0200

    Did Noam's changes.

--- src/main/java/frc/robot/Subsystems/ShooterSystem.java
+++ src/main/java/frc/robot/Subsystems/ShooterSystem.java
@@ -14,10 +14,10 @@ public class ShooterSystem extends Subsystem {
     private CANSparkMax follower;
     private RelativeEncoder encoder;
     // TODO: Figure out values for the 4 variables below.
-    private final double kf = 0;
-    private final double kd = 0;
-    private final double ki= 0;
-    private final double kp = 0;
+    private final double KF = 0;
+    private final double KD = 0;
+    private final double KI = 0;
+    private final double KP = 0;
     private PidController pid;
     private double ERROR = 0.05;
     private static final double SPEED_POINT = 0.5; // TODO: Figure ideal value for this variable.
@@ -28,24 +28,31 @@ public class ShooterSystem extends Subsystem {
         this.encoder = (RelativeEncoder) master.getEncoder();
         this.follower.follow(master, true); // TODO: Make sure this needs to be inverted.
         this.pid = new PidController(RunningRobot.getControl().getClock(),
-                ()-> SmartDashboard.getNumber("KP", kp),
-                ()-> SmartDashboard.getNumber("KI", ki),
-                ()-> SmartDashboard.getNumber("KD", kd),
-                ()-> SmartDashboard.getNumber("KF", kf));
-        pid.setOutputLimit(-0.7,0.7);
+                ()-> SmartDashboard.getNumber("KP", KP),
+                ()-> SmartDashboard.getNumber("KI", KI),
+                ()-> SmartDashboard.getNumber("KD", KD),
+                ()-> SmartDashboard.getNumber("KF", KF));
+        pid.setOutputLimit(-0.5,0.5);
         pid.setTolerance(ERROR, Time.milliseconds(500));
-        SmartDashboard.putNumber("KP", kp);
-        SmartDashboard.putNumber("KI", ki);
-        SmartDashboard.putNumber("KD", kd);
-        SmartDashboard.putNumber("KF", kf);
+        SmartDashboard.putNumber("KP", KP);
+        SmartDashboard.putNumber("KI", KI);
+        SmartDashboard.putNumber("KD", KD);
+        SmartDashboard.putNumber("KF", KF);
+    }
+
+    public double getPID(){
+        double speed = pid.applyAsDouble(getSpeed(),SPEED_POINT);
+
+        SmartDashboard.putNumber("Speed", speed);
+        return speed;
     }
 
     public void shoot(){
-        this.master.set(pid.applyAsDouble(getSpeed(),SPEED_POINT));
+        this.master.set(getPID());
     }
 
     public void reverse(){
-        this.master.set(-pid.applyAsDouble(getSpeed(),SPEED_POINT));
+        this.master.set(-getPID());
     }
 
     public double getSpeed(){
