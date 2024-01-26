8ad2c1ded1831c09f4eb77be3e9059f12b100092
talya.hacohen <talya.hacohen@gmail.com>
2024-01-26 21:43:17 +0200

    *** *btw '***' is for some fixes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,6 +4,6 @@ import com.flash3388.flashlib.frc.robot.RoboRio;
 import com.flash3388.flashlib.hid.HidChannel;
 
 public class RobotMap {
-    public static final int INTAKE = 0;
+    public static final int INTAKE = 13;
     public static final HidChannel XBOX = RoboRio.newHidChannel(0);
 }
