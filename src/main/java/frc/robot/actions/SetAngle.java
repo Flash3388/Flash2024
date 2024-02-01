0a263a02fd26fc9a831583f260baf59eade5de20
stavit456 <stavfrenkel456@gmail.com>
2024-02-01 19:45:41 +0200

    I added two new angles

--- src/main/java/frc/robot/actions/SetAngle.java
+++ src/main/java/frc/robot/actions/SetAngle.java
@@ -2,6 +2,7 @@ package frc.robot.actions;
 
 import com.flash3388.flashlib.scheduling.ActionControl;
 import com.flash3388.flashlib.scheduling.FinishReason;
+import com.flash3388.flashlib.scheduling.Requirement;
 import com.flash3388.flashlib.scheduling.actions.ActionBase;
 import frc.robot.subSystems.Arm;
 
@@ -9,9 +10,12 @@ import frc.robot.subSystems.Arm;
 public class SetAngle extends ActionBase {
 
     private Arm arm;
-
-    public SetAngle(Arm arm){
+    private boolean high;
+    private boolean low;
+    public SetAngle(Arm arm, boolean high, boolean low){   //high true
         this.arm = arm;
+        this.high = true;
+        this.low = false;
 
         requires(arm);
     }
@@ -24,7 +28,12 @@ public class SetAngle extends ActionBase {
 
     @Override
     public void execute(ActionControl control) {
-        arm.move();
+        if(high){
+            arm.moveHigh();
+        }
+        else{
+            arm.moveLow();
+        }
     }
 
     @Override
--- src/main/java/frc/robot/subSystems/Arm.java
+++ src/main/java/frc/robot/subSystems/Arm.java
@@ -19,6 +19,9 @@ public class Arm extends Subsystem {
     private static final double KF = 0;
     private static final double ERROR = 1;
     private static final double LIMIT = 1;
+    private static final double SPEED = 0.2;
+    private static final double HIGH = 40;
+    public static final double LOW = 20;
 
 
     public Arm(double angle2Target, CANSparkMax master, CANSparkMax follower, DutyCycleEncoder encoder){
@@ -44,15 +47,23 @@ public class Arm extends Subsystem {
         return encoder.getAbsolutePosition() * 360;
     }
 
-    public double speed(){
+    public double SpeedHigh(){
+        angle2Target = HIGH;
         return pid.applyAsDouble(getAngle2Target(), angle2Target);
     }
-    public void move(){
-        master.set(speed());
+    public double SpeedLow(){
+        angle2Target = LOW;
+        return pid.applyAsDouble(getAngle2Target(), angle2Target);
+    }
+    public void moveHigh(){
+        master.set(SpeedHigh());
+    }
+    public void moveLow(){
+        master.set(SpeedLow());
     }
     public void pidReset(){
         pid.reset();
     }
 
-    public void stop(){ master.stopMotor();}
+    public void stop(){ master.set(SPEED);}
 }
