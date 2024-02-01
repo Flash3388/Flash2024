e94e63797dc014fe7c8e6615a292120d19369129
stavit456 <stavfrenkel456@gmail.com>
2024-02-01 18:23:29 +0200

    Fixed the SystemFactory.

--- src/main/java/frc/robot/SystemFactory.java
+++ src/main/java/frc/robot/SystemFactory.java
@@ -7,7 +7,7 @@ import frc.robot.subSystems.Arm;
 
 public class SystemFactory {
     public static Arm createArm(){
-        CANSparkMax master = new CANSparkMax(RobotMap.ARM_MASTER, CANSparkMaxLowLevel.MotorType.kBrushed);
+        CANSparkMax master = new CANSparkMax(RobotMap.ARM_MASTER, CANSparkMaxLowLevel.MotorType.kBrushless);
         CANSparkMax follow = new CANSparkMax(RobotMap.ARM_FOLLOW, CANSparkMaxLowLevel.MotorType.kBrushless);
         DutyCycleEncoder encoder = new DutyCycleEncoder(RobotMap.ARM_ENCODER);
         return new Arm(encoder.getAbsolutePosition(), master, follow, encoder);
