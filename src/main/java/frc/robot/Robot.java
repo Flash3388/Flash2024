97d6bac596f529d774c42fdad03bb538f5d63dd8
Maayan <d46880@ganyavne.ort.org.il>
2024-02-05 21:19:58 +0200

    pid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,9 +3,13 @@ package frc.robot;
 import com.flash3388.flashlib.frc.robot.FrcRobotControl;
 import com.flash3388.flashlib.frc.robot.base.iterative.DelegatingFrcRobotControl;
 import com.flash3388.flashlib.frc.robot.base.iterative.IterativeFrcRobot;
+import com.flash3388.flashlib.robot.control.PidController;
 import com.flash3388.flashlib.scheduling.actions.ActionGroup;
+import com.jmath.ExtendedMath;
 import com.revrobotics.*;
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,8 +44,8 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
     // as it will allow increased acceleration for the arm and thus cause it to accelerate quicly.
 
     // for motor PID
-    private static  double KP = 0; // tune this
-    private static  double KI = 0; // tune this
+    private static  double KP = 0.01; // tune this
+    private static  double KI = 0.0014; // tune this
     private static  double KD = 0; // tune this
     private static  double I_ZONE = 0; // tune this
 
@@ -62,16 +66,10 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
 
     private final CANSparkMax motor;
     private final CANSparkMax follower;
-    private final SparkPIDController motorPid;
-    private final TrapezoidProfile motorPositionProfile;
-    private final ArmFeedforward motorFeedForward;
-    private final RelativeEncoder relativeEncoder1;
-    private final RelativeEncoder relativeEncoder2;
+    private ArmFeedforward motorFeedForward;
     private final DutyCycleEncoder absEncoder;
 
-    private TrapezoidProfile.State currentMotorSetPoint;
-    private TrapezoidProfile.State endMotorSetPoint;
-
+    private ProfiledPIDController pid;
 
     public Robot(FrcRobotControl robotControl) {
         super(robotControl);
@@ -79,11 +77,7 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
         follower = new CANSparkMax(16, CANSparkLowLevel.MotorType.kBrushless);
         follower.follow(motor, true);
 
-        motorPid = motor.getPIDController();
-        motorPositionProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION));
         motorFeedForward = new ArmFeedforward(STATIC_GAIN, GRAVITY_GAIN, VELOCITY_GAIN);
-        relativeEncoder1 = motor.getEncoder();
-        relativeEncoder2 = follower.getEncoder();
 
 
 
@@ -92,29 +86,21 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
         absEncoder.setPositionOffset(81.79668 / 360);
 
      // I have found out that the method "absEncoder.getAbsolutePosition()" returns 0 in the constructor. this must be the problem.
-          relativeEncoder1.setPosition(-7.68 / GEAR_RATIO / 360 );
           SmartDashboard.putNumber("Abs positon start", (absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset()) *360);
-          relativeEncoder2.setPosition(-7.68 / GEAR_RATIO / 360);
 
-         SmartDashboard.putNumber("ARM P Gain", 0.0);
-         SmartDashboard.putNumber("ARM I Gain", 0);
-         SmartDashboard.putNumber("ARM D Gain", 0);
-         SmartDashboard.putNumber("ARM IZ Zone", 0);
+         SmartDashboard.putNumber("ARM P Gain", KP);
+         SmartDashboard.putNumber("ARM I Gain", KI);
+         SmartDashboard.putNumber("ARM D Gain", KD);
+         SmartDashboard.putNumber("ARM IZ Zone", I_ZONE);
          SmartDashboard.putNumber("ARM Static Gain", 0);
          SmartDashboard.putNumber("ARM Gravity", 0);
 
          SmartDashboard.putNumber("ARM Max Velocity", 0.01);
-         SmartDashboard.putNumber("ARM Velocity Gain", 0.01);
-         SmartDashboard.putNumber("ARM Max Acceleration", 0.001);
+         SmartDashboard.putNumber("ARM Velocity Gain", 0.0);
+         SmartDashboard.putNumber("ARM Max Acceleration", 0.071);
 
 
-
-        motorPid.setP(KP);
-        motorPid.setI(KI);
-        motorPid.setD(KD);
-        motorPid.setIZone(I_ZONE);
-        motorPid.setFF(0); // don't use the motor's feed forward, we will supply our own
-        motorPid.setOutputRange(-1, 1);
+        pid = new ProfiledPIDController(KP, KI, KD, new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION));
     }
 
     @Override
@@ -129,7 +115,6 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
 
     @Override
     public void teleopInit() {
-
     }
 
     @Override
@@ -139,44 +124,27 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
 
     @Override
     public void autonomousInit() {
-        // set wanted final setpoint for the motor, 70 degrees, and stop at 0 velocity.
-       // endMotorSetPoint = new TrapezoidProfile.State(50.0, 0);
-        endMotorSetPoint = new TrapezoidProfile.State(9.0, 0);
-        // set this to the current position (angle) and velocity (0).
-        // for real robot, use the position supplied by the encoder
-       // currentMotorSetPoint = new TrapezoidProfile.State(getArmPosition(), 0);
-        currentMotorSetPoint = new TrapezoidProfile.State(getRelativeArmPosition() * GEAR_RATIO * 360, 0);
     }
 
     @Override
     public void autonomousPeriodic() {
-        // this will calculate our next setPoint for the motor,
-        currentMotorSetPoint = motorPositionProfile.calculate(LOOP_TIME_SECONDS, currentMotorSetPoint, endMotorSetPoint);
-        // currentMotorSetPoint.position == position [degrees] to move the arm to
-        // currentMotorSetPoint.velocity == velocity [degrees/second] to move the arm at
-
-        // this will provide a feed-forward
-        double feedForward = motorFeedForward.calculate(Math.toRadians(currentMotorSetPoint.position), currentMotorSetPoint.velocity);
-        // feedForward = volts to add to the motor output (if PID says output=5, then the real output will be 5+feedForward)
-
         changePidValues();
-        SmartDashboard.putNumber("SET POINTS", currentMotorSetPoint.position);
-
-
-        // feed the info to the motor
-        double positionForMotor = currentMotorSetPoint.position / 360.0 / GEAR_RATIO;
-        motorPid.setReference(
+        /*motorPid.setReference(
                 positionForMotor,
                 CANSparkBase.ControlType.kPosition,
                 PID_SLOT,
                 feedForward,
-                SparkPIDController.ArbFFUnits.kVoltage);
+                SparkPIDController.ArbFFUnits.kVoltage);*/
 
+       // motorPid.setReference(9 / GEAR_RATIO / 360.0, CANSparkBase.ControlType.kPosition);
 
-        SmartDashboard.putNumber("Arm position", getArmPosition());
+        double speed = pid.calculate(getArmPosition(), 10);
+        speed = ExtendedMath.constrain(speed, -0.5, 0.5);
+        SmartDashboard.putNumber("MOTOR Set Point", speed);
+        // SmartDashboard.putNumber("MOTOR Feed Forward", feedForward);
+        motor.set(speed);
+
        // SmartDashboard.putNumber("OFFSET", absEncoder.getAbsolutePosition() * 360);
-
-        SmartDashboard.putNumber("GetRelativePosition", getRelativeArmPosition() * 360 * GEAR_RATIO);
     }
 
     private double getArmPosition(){
@@ -185,21 +153,6 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
 
       //  return (absEncoder.getAbsolutePosition()-absEncoder.getPositionOffset()) * 360;
         return (absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset()) * 360;
-        //return (absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset())  * 360;
-    }
-
-    private double getRelativeArmPosition(){
-        relativeEncoder1.setPosition((absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset()) / GEAR_RATIO);
-        relativeEncoder2.setPosition((absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset()) / GEAR_RATIO);
-
-        SmartDashboard.putNumber("rel position 1", relativeEncoder1.getPosition() * GEAR_RATIO * 360);
-        SmartDashboard.putNumber("rel position 2", relativeEncoder2.getPosition() * GEAR_RATIO * 360);
-
-        double avgRelativeEncoder = (relativeEncoder2.getPosition() + relativeEncoder1.getPosition()) /2;
-
-        SmartDashboard.putNumber("avg rel position", avgRelativeEncoder);
-
-        return avgRelativeEncoder;
         //return (absEncoder.getAbsolutePosition() - absEncoder.getPositionOffset())  * 360;
     }
 
@@ -217,16 +170,23 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
         double maxA = SmartDashboard.getNumber("ARM Max Acceleration", .01);
 
         // if PID coefficients on SmartDashboard have changed, write new values to controller
-        if((p != KP)) { motorPid.setP(p); KP = p; }
-        if((i != KI)) { motorPid.setI(i); KI = i; }
-        if((d != KD)) { motorPid.setD(d); KD = d; }
-        if((iz != I_ZONE)) { motorPid.setIZone(iz); I_ZONE = iz; }
-        
-        if((maxV != MAX_VELOCITY)) {  MAX_VELOCITY = maxV; }
-        if((velocityG != VELOCITY_GAIN)) {  VELOCITY_GAIN = velocityG; }
-        if((maxA != MAX_ACCELERATION)) { MAX_ACCELERATION = maxA; }
-        if((staticGain != STATIC_GAIN)) { STATIC_GAIN = staticGain; }
-        if((gravityG != GRAVITY_GAIN)) { GRAVITY_GAIN = gravityG; }
+        if((p != KP)) { pid.setP(p); KP = p; }
+        if((i != KI)) { pid.setI(i); KI = i; }
+        if((d != KD)) { pid.setD(d); KD = d; }
+        if((iz != I_ZONE)) { pid.setIZone(iz); I_ZONE = iz; }
+
+        if ((maxV != MAX_VELOCITY) || (maxA != MAX_ACCELERATION)) {
+            MAX_VELOCITY = maxV;
+            MAX_ACCELERATION = maxA;
+            pid.setConstraints(new TrapezoidProfile.Constraints(maxV, maxA));
+        }
+
+        if((staticGain != STATIC_GAIN) || (gravityG != GRAVITY_GAIN) || (velocityG != VELOCITY_GAIN)) {
+            STATIC_GAIN = staticGain;
+            GRAVITY_GAIN = gravityG;
+            VELOCITY_GAIN = velocityG;
+            motorFeedForward = new ArmFeedforward(STATIC_GAIN, GRAVITY_GAIN, VELOCITY_GAIN);
+        }
     }
 
     @Override
@@ -241,6 +201,7 @@ public class Robot extends DelegatingFrcRobotControl implements IterativeFrcRobo
 
     @Override
     public void robotPeriodic() {
+        SmartDashboard.putNumber("ABS Arm position", getArmPosition());
     }
 
     @Override
